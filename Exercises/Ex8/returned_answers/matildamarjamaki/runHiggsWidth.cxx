edbf19cce6a8b1a98e79562024d8e1b5072f110e
matildamarjamaki <matilda.marjamaki@helsinki.fi>
2025-03-19 13:42:00 +0200

    Ex8

--- Exercises/Ex8/returned_answers/matildamarjamaki/runHiggsWidth.cxx
+++ Exercises/Ex8/returned_answers/matildamarjamaki/runHiggsWidth.cxx
@@ -7,17 +7,13 @@ using namespace Pythia8;
 int main() {
     Pythia pythia;
     
-    // Set SM Higgs boson mass
-    pythia.readString("Higgs:useBSM = off");  // Ensure SM Higgs is used
-    pythia.readString("25:m0 = 125.0");       // Higgs mass (GeV)
+    pythia.readString("Higgs:useBSM = off");
+    pythia.readString("25:m0 = 125.0");
 
-    // Initialize Pythia
     pythia.init();
 
-    // Retrieve the Higgs boson width
     double higgsWidth = pythia.particleData.mWidth(25);
 
-    // Print result
     std::cout << "SM Higgs boson width for m_H = 125 GeV: " 
               << higgsWidth << " GeV" << std::endl;
 
